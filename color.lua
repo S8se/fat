51e3127584e61d70469644e94ade5ced69b79ae1
ram4explosion <94090198+S8se@users.noreply.github.com>
2022-03-28 16:40:56 +0700

    Update color.lua

--- color.lua
+++ color.lua
@@ -1,47 +1,67 @@
-    local TS = game:GetService("TweenService")
-    local Speed = 250
-    function ToSnowwhite()
+local TS = game:GetService("TweenService")
+local Speed = 250
+nc = false
+spawn(function()
+    while true do
+    LocalPlayer = game.Players.LocalPlayer
+    Char = LocalPlayer.Character
+    if nc then
+        for _, v in pairs(Char:GetDescendants()) do
+            pcall(function()
+                if v:IsA("BasePart") then
+                    v.CanCollide = false
+                end
+            end)
+        end
+    end
+    game:GetService("RunService").Stepped:wait()
+end
+end)
+function ToColor(hello)
+    nc = true
+    hallo = {
+        'Snow White',
+        'Winter Sky',
+        'Pure Red'
+    }
+    if hello == "White" then
+        local l1 = {
+            [1] = "activateColor",
+            [2] = hallo[1]
+        }
+        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(l1));
         Time = (Vector3.new(-4971.47559, 331.565765, -3720.02954) - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude / Speed
         Info = TweenInfo.new(Time, Enum.EasingStyle.Linear)
         Tween = TS:Create(game.Players.LocalPlayer.Character.HumanoidRootPart,Info,{CFrame = CFrame.new(Vector3.new(-4971.47559, 331.565765, -3720.02954))})
         Tween:Play()
         Tween.Completed:Wait()
-    end
-    function ToWintersky()
-        Time = (Vector3.new(-5420.16602, 1084.9657, -2666.82) - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude / Speed
+    elseif hello == "Pink" then
+        local l1 = {
+            [1] = "activateColor",
+            [2] = hallo[2]
+        }
+        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(l1));
+        Time = (Vector3.new(-5420.17, 1084.97, -2666.82) - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude / Speed
         Info = TweenInfo.new(Time, Enum.EasingStyle.Linear)
-        Tween = TS:Create(game.Players.LocalPlayer.Character.HumanoidRootPart,Info,{CFrame = CFrame.new(Vector3.new(-5420.16602, 1084.9657, -2666.8208))})
+        Tween = TS:Create(game.Players.LocalPlayer.Character.HumanoidRootPart,Info,{CFrame = CFrame.new(Vector3.new(-5420.17, 1084.97, -2666.82))})
         Tween:Play()
         Tween.Completed:Wait()
-    end
-    function toPureRedAndPrint()
-        a = "%x"
-        b = "%X"
+    elseif hello == "Red" then
+        local l1 = {
+            [1] = "activateColor",
+            [2] = hallo[3]
+        }
+        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(l1));
         Time = (Vector3.new(-5414.41357, 309.865753, -2212.45776) - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude / Speed
         Info = TweenInfo.new(Time, Enum.EasingStyle.Linear)
         Tween = TS:Create(game.Players.LocalPlayer.Character.HumanoidRootPart,Info,{CFrame = CFrame.new(Vector3.new(-5414.41357, 309.865753, -2212.45776))})
         Tween:Play()
-        Tween.Completed:Connect(function()
-            warn("Activate Color finished at", os.date(a), os.date(b));
-        end);
-    end;
-    local l1 = {
-        [1] = "activateColor",
-        [2] = "Snow White"
-    };
-    game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(l1));
-    ToSnowwhite()
-    wait()
-    local l2 = {
-        [1] = "activateColor",
-        [2] = "Winter Sky"
-    };
-    game.ReplicatedStorage.Remotes.CommF_:InvokeServer(unpack(l2));
-    ToWintersky();
-    wait()
-    local l3 = {
-        [1] = "activateColor",
-        [2] = "Pure Red"
-    };
-    game.ReplicatedStorage.Remotes.CommF_:InvokeServer(unpack(l3));
-    toPureRedAndPrint();
+        Tween.Completed:Wait()
+    end
+    nc = false
+end
+ToColor("White")
+wait()
+ToColor("Pink")
+wait(.1)
+ToColor("Red")
